79a1fc8cbbb8422c3ece02a8cebcc63497a683b9
Saarah Kothawala <saarahsk@gmail.com>
2019-05-15 18:01:25 -0700

    remove check in sem_getvalue -- sem->waiting will always be created by sem_create and never be null -- if there are no threads waiting, queue_length will return 0 and sval will be 0

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -114,11 +114,6 @@ int sem_getvalue(sem_t sem, int *sval)
   }
 
   if (sem->count == 0){
-    if (!sem->waiting){
-      fprintf(stderr, "There are no waiting threads!\n");
-      return -1;
-    }
-
     *sval = queue_length(sem->waiting) * -1;
     return 0;
   }

72815ccf869224c3d0480e6bffe242c319fb98ff
Saarah Kothawala <saarahsk@gmail.com>
2019-05-15 19:50:37 -0700

    do tps

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -7,35 +7,180 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <math.h>
 
 #include "queue.h"
 #include "thread.h"
 #include "tps.h"
 
-/* TODO: Phase 2 */
+// make it easy to look up a particular thread's tps data with a table
+#define MAX_THREADS 256
+
+struct tps {
+  void* data;
+};
+
+struct tps tps_table[MAX_THREADS];
+
+static pthread_t get_tid()
+{
+  // ensure that we don't accidentally index too far into the tps table
+  pthread_t tid = pthread_self();
+  if (tid >= MAX_THREADS) {
+    fprintf(stderr, "tps library only supports a maximum of %d threads\n",
+        MAX_THREADS);
+    abort();
+  }
+
+  return tid;
+}
+
+// return 0 if the tid has a tps
+static int tid_has_tps(pthread_t tid) {
+  // data == NULL if the tps for thread tid doesn't exist
+  if (tps_table[tid].data == NULL) {
+    return -1;
+  }
+
+  return 0;
+}
+
+// returns 0 if initial checks on this io operation are valid, -1 otherwise
+static int tps_io_check(size_t offset, size_t length, char* buffer) {
+  pthread_t tid = get_tid();
+
+  int ret = tid_has_tps(tid);
+  if (ret == 0) {
+    return -1;
+  }
+
+  // check whether the read operation within bounds
+  if (offset + length >= TPS_SIZE) {
+    return -1;
+  }
+
+  if (buffer == NULL) {
+    return -1;
+  }
+
+  return 0;
+}
+
+static int tps_mmap_set_prot(int prot) {
+  pthread_t tid = get_tid();
+
+  int flags = MAP_FIXED | MAP_PRIVATE | MAP_ANONYMOUS;
+  void* ret = mmap(tps_table[tid].data, TPS_SIZE, prot, flags, 0, 0);
+  if (ret == MAP_FAILED) {
+    return -1;
+  }
+
+  tps_table[tid].data = ret;
+  return 0;
+}
+
+static int tps_enable_read(void) {
+  return tps_mmap_set_prot(PROT_READ);
+}
+
+static int tps_enable_write(void) {
+  return tps_mmap_set_prot(PROT_WRITE);
+}
+
+static int tps_disable_read_write(void) {
+  return tps_mmap_set_prot(PROT_NONE);
+}
 
 int tps_init(int segv)
 {
+  // protect from reinitialization
+  static int initialized = 0;
+  if (initialized != 0) {
+    return -1;
+  }
+  initialized = 1;
+
+  for (int i = 0; i < MAX_THREADS; i++) {
+    tps_table[i].data = NULL;
+  }
+
+  if (segv == 0) {
+   // user didn't ask for segv handling, nothing more to do here
+    return 0;
+  }
+
   return 0;
 }
 
 int tps_create(void)
 {
+  pthread_t tid = get_tid();
+
+  // data != NULL if the tps for this thread already exists
+  if (tps_table[tid].data != NULL) {
+    return -1;
+  }
+
+  // MAP_ANONYMOUS: This flag tells the system to create an anonymous mapping,
+  // not connected to a file. filedes and offset are ignored, and the region is
+  // initialized with zeros.
+  //
+  // MAP_PRIVATE: a copy is made for th eprocess and no other process will see
+  // the changes.
+  //
+  // mmap already fills the data region with zeros so no need to do that.
+  void* data = mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0);
+  if (data == MAP_FAILED) {
+    return -1;
+  }
+
+  tps_table[tid].data = data;
   return 0;
 }
 
 int tps_destroy(void)
 {
+  pthread_t tid = get_tid();
+
+  int ret = tid_has_tps(tid);
+  if (ret == 0) {
+    return -1;
+  }
+
+  int ret = munmap(tps_table[tid].data, TPS_SIZE);
+  if (ret == -1) {
+    return -1;
+  }
+
+  tps_table[tid].data = NULL;
   return 0;
 }
 
 int tps_read(size_t offset, size_t length, char *buffer)
 {
+  int ret = tps_io_check(offset, length, buffer);
+  if (ret == -1) {
+    return -1;
+  }
+
+  pthread_t tid = get_tid();
+  tps_enable_read();
+  memcpy(buffer, tps_table[tid].data + offset, length);
+  tps_disable_read_write();
   return 0;
 }
 
 int tps_write(size_t offset, size_t length, char *buffer)
 {
+  int ret = tps_io_check(offset, length, buffer);
+  if (ret == -1) {
+    return -1;
+  }
+
+  pthread_t tid = get_tid();
+  tps_enable_write();
+  memcpy(tps_table[tid].data + offset, buffer, length);
+  tps_disable_read_write();
   return 0;
 }
 
